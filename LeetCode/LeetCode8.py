16c42ee40e5fbf69eaea42e2ab7b77a98a53610e
Himanshu Pant <Himanshu.Pant@walmart.com>
2024-05-22 19:13:15 +0530

    Leet code 7,8

---
+++ LeetCode/LeetCode8.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        for i in range(len(prices)):
+            prices[i]
+
+
+        return prices
+
+
+prices = [7,1,5,3,6,4]
+sol = Solution()
+print(sol.maxProfit(prices))
